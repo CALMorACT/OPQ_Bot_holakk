857fbba73c4a1b6bcf5d569b2ec7d85d94061bcf
holakk <2646677495@qq.com>
2020-10-23 23:31:06 +0800

    fix(pic_base64): change to aria2 download

--- remind_daka/get_nodaka_usr.py
+++ remind_daka/get_nodaka_usr.py
@@ -3,6 +3,7 @@ import os
 import base64
 import random
 import string
+import subprocess
 import time
 from urllib.parse import urlencode
 from PIL import Image
@@ -24,14 +25,15 @@ class TXApiUse:
         self.sign = ""
 
     def get_pic_base64(self):
-        headers = {
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.11 Safari/537.36 Edg/87.0.664.8',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-        }
-        response_download = requests.request("GET", self.image_url, headers=headers)
-        with open("temp.png", "wb") as code:
-            code.write(response_download.content)
+        # headers = {
+        #     'Upgrade-Insecure-Requests': '1',
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.11 Safari/537.36 Edg/87.0.664.8',
+        #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        # }
+        # response_download = requests.request("GET", self.image_url, headers=headers)
+        # with open("temp.png", "wb") as code:
+        #     code.write(response_download.content)
+        subprocess.call(['aria2c', self.image_url, '-o', 'temp.png'])
         img = Image.open("temp.png")
         w, h = img.size
         cropped = img.crop((0, int(h * 0.1), int(w * 0.16), int(h * 0.95)))
